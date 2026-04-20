63be0bb4e7f47b20a31e26f31e58e85f00cf85e0
ahmeddev812 <mahmedfaisal2005@gmail.com>
2026-04-20 16:28:17 +0500

    updated: proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,32 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/onboarding",
-]);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
-
-  // Define the Content Security Policy header
-  // Note: 'unsafe-eval' is included to allow libraries that require string evaluation.
-  const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.clerk.com clerk.com;
-    connect-src 'self' *.clerk.com clerk.com;
-    img-src 'self' data: img.clerk.com;
-    worker-src 'self' blob:;
-    style-src 'self' 'unsafe-inline';
-    frame-src 'self' *.clerk.com clerk.com;
-  `.replace(/\s{2,}/g, ' ').trim();
-
-});
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)"],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
