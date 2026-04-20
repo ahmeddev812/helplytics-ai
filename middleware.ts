e7d3482bed18fe25c2dd77cbd8fe3b7a141d3c99
ahmeddev812 <mahmedfaisal2005@gmail.com>
2026-04-20 15:24:41 +0500

    resolved errors

--- middleware.ts
+++ middleware.ts
@@ -4,16 +4,12 @@ const isPublicRoute = createRouteMatcher([
   "/",
   "/sign-in(.*)",
   "/sign-up(.*)",
+  "/onboarding",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const session = await auth();
-  const userId = session.userId;
-
-  const isPublic = isPublicRoute(req);
-
-  if (!userId && !isPublic) {
-    return Response.redirect(new URL("/sign-in", req.url));
+  if (!isPublicRoute(req)) {
+    await auth.protect();
   }
 });
 
